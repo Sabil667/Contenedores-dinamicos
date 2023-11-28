1d27de23402b8965c637e98027dd347e3691330a
Sabil667 <msabi@myuax.com>
2023-11-28 11:52:49 +0100

    .

--- EJ1/EJ1.cpp
+++ EJ1/EJ1.cpp
@@ -4,9 +4,19 @@
 using namespace std;
 
 int main(){
-    vector<int> numeros=(10,20,30,40,50,60);
+    vector<int> numeros;
+    numeros.push_back(1);
+    numeros.push_back(2);
+    numeros.push_back(3);
+    numeros.push_back(4);
+    numeros.push_back(5);
+
 
     cout<<"Los elementos del vector son: ";
+    for(vector<int>::iterator it=numeros.begin();it!=numeros.end();++it){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
 
 
 
